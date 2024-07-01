c3f1473da8406f7a2649f7a2279d212821f1c82d
Miliya Immatty Lazar <miliyaninu@gmail.com>
2024-07-01 19:02:19 +1000

    completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+/*const strings=[]
+let newArray=[]
+function writecards(strings,eventName){
+    for(i=0;i<strings.length;i++)
+    {
+        console.log(`Thankyou ${strings[i]} for wonderful ${eventName} gift`)
+        
+         newArray.push(`Thankyou ${strings[i]} for wonderful ${eventName} gift`)
+        
+    }
+    console.log(newArray)
+
+}
+writecards(["Charlie", "Samip", "Ali"], "birthday")*/
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
+
+countDown(10);
